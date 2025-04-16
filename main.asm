a1944db0dcfea662bcc42ace3aecc473ff26f613
Gabriel Glez <gabrielgg85@gmail.com>
2025-04-16 16:49:36 +0100

    ok done! , print by param

--- main.asm
+++ main.asm
@@ -2,7 +2,7 @@ BasicUpstart2(main)
 
 #import "print/print_char.asm"
 
-message: .text @"hola que tal como estan\$00"
+message: .text @"holaaaa que tal como estan\$00"
 //message: .byte 'h'
 
 main:
@@ -16,13 +16,14 @@ main:
     lda #5
     sta SCREEN_COL_POS
 
-    //jsr print_char  // print single char
+    jsr print_char  // print single char
     
     jsr clean_location_screen
     lda #4
     sta SCREEN_ROW_POS
     lda #8 
     sta SCREEN_COL_POS
+
 
     lda #<message
     sta ZERO_PAGE_PRINT_TEXT_LO
@@ -36,15 +37,15 @@ rts
 
 print_text:
 
-    ldx #0
+    ldy #0
     continue_writing:
 
         //lda message,x
-        lda (ZERO_PAGE_PRINT_TEXT_LO),x
-        beq end_writing  //si A == 0 flag Z active
-        sta SCREEN_CHAR //load char to show
+        lda (ZERO_PAGE_PRINT_TEXT_LO),y
+        beq end_writing                    //si A == 0 flag Z active
+        sta SCREEN_CHAR                    //load char to show
         jsr print_char
-        inx
+        iny
         inc SCREEN_COL_POS
         jmp continue_writing
 
