88ee1b47e3b41242242ba9ea8a946763b29587fe
Gabriel Glez <gabrielgg85@gmail.com>
2025-04-25 22:34:23 +0100

    working on macro ...

--- demos/math_lib/sum_32_bits.asm
+++ demos/math_lib/sum_32_bits.asm
@@ -1,10 +1,10 @@
 
 // 65765 + 89927 = 155692
 
-lda #$05
+lda #$0A
 sta sum_num1_0
 
-lda #$10
+lda #$00
 sta sum_num1_1
 
 lda #$00
@@ -14,7 +14,7 @@ lda #$00
 sta sum_num1_3
 
 
-lda #$02
+lda #$00
 sta sum_num2_0
 
 lda #$00
@@ -49,9 +49,9 @@ jsr MATH_LIB.sum_32
 //Prepare result to print in screen
 //We need save this value on a division by 10
 
-
-//.break
 //N1
+//Load the result of operation ... + - / *
+//to the DIV_num1_result
 lda sum_res_0
 sta div_num1_0
 
@@ -64,63 +64,14 @@ sta div_num1_2
 lda sum_res_3
 sta div_num1_3
 
-//N2
-lda #10
-sta div_num2_0
-lda #0
-sta div_num2_1
-sta div_num2_2
-sta div_num2_3
 
-sta div_res_0
-sta div_res_1
-sta div_res_2
-sta div_res_3
-
-
-
-
-ldx #0              // índice para guardar restos en la tabla
-loop_digits:
-
-    //clean result in each loop
-    lda #0
-    sta div_res_0
-    sta div_res_1
-    sta div_res_2
-    sta div_res_3
-
-    jsr MATH_LIB.division_32 
-
-    //save module
-    lda div_num1_0
-    sta NUMBER_TO_PRINT_TABLE,x
-    inx
-    inc total_digits
-
-    lda div_res_0
-    sta div_num1_0
-
-    lda div_res_1
-    sta div_num1_1
-
-    lda div_res_2
-    sta div_num1_2
-
-    lda div_res_3
-    sta div_num1_3
-
-    lda div_res_0
-    bne loop_digits
-
-    // Imprimir mensaje
+// Imprimir mensaje
     jsr PRINT_LIB.clean_location_screen
     locate_text(5,0,WHITE)
     print_text(sum_result_str)
 
     jsr PRINT_LIB.print_number
 
-    //.break
 
 
 /*
--- libraries/print_lib/print_lib.asm
+++ libraries/print_lib/print_lib.asm
@@ -131,41 +131,148 @@ print_number:
     tya  // transfer Y to A
     pha  // push A (Y) to stack
 
+
+    // Prepare N2 . Set number to 10
+    // because it is need it to get 
+    // the modules, ( digits )
+    lda #10
+    sta div_num2_0
+    lda #0
+    sta div_num2_1
+    sta div_num2_2
+    sta div_num2_3
+
+    //set to 0 the div_resX
+    sta div_res_0
+    sta div_res_1
+    sta div_res_2
+    sta div_res_3
+
+
+    // set to 0 X , it is the index to save
+    // each digit ( module ) on the table
+    ldx #0              
+    loop_digits:
+
+        //clean result in each loop
+        lda #0
+        sta div_res_0
+        sta div_res_1
+        sta div_res_2
+        sta div_res_3
+
+        // divide by 10
+        jsr MATH_LIB.division_32 
+
+        //save module
+        lda div_num1_0
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx // increment X to prepare the next position 
+            // on table
+
+        inc total_digits // increment the max available digits
+
+        // update quotient for next loop
+        // update the result of the division
+        // for the next iteration
+        lda div_res_0
+        sta div_num1_0
+
+        lda div_res_1
+        sta div_num1_1
+
+        lda div_res_2
+        sta div_num1_2
+
+        lda div_res_3
+        sta div_num1_3
+
+        lda div_res_0
+        bne loop_digits
+
+    // when all modules are saved its time 
+    // to show the digits
+
+    // start from the tail of the TABLE because
+    // the number are ordered on reverse.
+    // where we read the tail from right to left
+    // until see the first digits.
+    // the NUMBER_TO_PRINT_TABLE can be like:
+    // 04 05 06 03 05 00 00 00 00
     read_tail:
-        dec counter_table
-        ldx counter_table
-        lda NUMBER_TO_PRINT_TABLE,x
-        beq read_tail
+        
+        dec counter_table   // counter--
+        ldx counter_table   // load X with new counter value
+        lda NUMBER_TO_PRINT_TABLE,x //get next module to show
+        beq read_tail //
         //when do not found a 0 ... the number started
-    
+
+    // when the first number appears
+    // its time to put the digits on the
+    // varaible "number_to_print_str".
+    // this variable it is used for
+    // save the numbers.
+
+    // start counter_str 0
     lda #0
     sta counter_str
     ldy counter_str
     copy_number:
 
+        // we continue reading where now are numbers
         lda NUMBER_TO_PRINT_TABLE,x
         clc
-        adc #$30 //get char
-        sta number_to_print_str,y
+        adc #$30 //add $30 to get the CHAR_CODE to see the digit
+        sta number_to_print_str,y // and save the char on the varible
         
-        inc counter_str
-        ldy counter_str
+        inc counter_str  // increment counter_str
+        ldy counter_str  // load the new value on Y
 
-        dec counter_table
-        ldx counter_table
+        dec counter_table // decrement counter table
+        ldx counter_table // update X with new counter value
 
-        dec total_digits
-        //lda counter_ta
-        lda total_digits
-        bne copy_number
+        dec total_digits  // this value contains the total of real digits
+                          // without 0 digits
+        lda total_digits  // load the current value of total digits
+        bne copy_number   // if not is 0 , continue
 
-        //reset total_digits for next print number
-        lda #0
-        sta total_digits
         // Print number
         jsr PRINT_LIB.clean_location_screen
         locate_text(8,0,WHITE)
         print_text(number_to_print_str)
+
+
+        // reset all for next print digit
+        //reset total_digits for next print number
+        lda #0
+        sta total_digits
+        sta counter_str
+
+        lda #10
+        sta counter_table
+
+        lda #0
+        ldx #0
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+        inx
+        sta NUMBER_TO_PRINT_TABLE,x
+
 
         pla // pull A from stack (Y)
         tay // transfer A to Y
