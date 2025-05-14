c19bc3cc88a709a4ae615750dc489d610119c0dd
Gabriel Glez <gabrielgg85@gmail.com>
2025-05-14 12:11:23 +0100

    new approach. check 2 keys. Beta 1.0

--- libraries/input_lib/input_lib.asm
+++ libraries/input_lib/input_lib.asm
@@ -4,6 +4,7 @@ INPUT_LIB:
     #import "input_macros/input_lib_macros.asm"
 
     input_keyboard:
+
         // Port A = enter
         lda #$00
         sta $DC02 //; ---> $DC00
@@ -13,81 +14,85 @@ INPUT_LIB:
         sta $DC03 //; ---> $DC01
 
     read_key:
+
         jsr clear_key_pressed_table
         
         // Primer paso: escanear todas las teclas sin imprimir nada
         jsr scan_all_keys
         
         // Segundo paso: imprimir resultados de las teclas detectadas
-        jsr print_key_results
+        jsr process_pressed_keys
         
         jsr sleep_key    // Un solo sleep después de todas las impresiones
         jmp read_key
 
     scan_all_keys:
+        
         ldx #0
-    scan_rows_loop:
-        lda TABLE_KEY_BOARD_ROW,x
-        sta $DC01
+        scan_rows_loop:
 
-        lda $DC00
-        eor #%11111111
-        sta KEY_PRESSED
+            lda TABLE_KEY_BOARD_ROW,x
+            sta $DC01
 
-        ldy #0
-    
-    scan_cols_loop:
+            lda $DC00
+            eor #%11111111
+            sta KEY_PRESSED
 
-        lda KEY_PRESSED
-        and TABLE_KEY_BOARD_COL,y
-        beq no_key_detected
+            ldy #0
+            scan_cols_loop:
 
-        // Calcular offset y guardar en la tabla
-        stx TEMP_X_REG    // Guardar X temporalmente
-        sty TEMP_Y_REG    // Guardar Y temporalmente
+                lda KEY_PRESSED
+                and TABLE_KEY_BOARD_COL,y
+                beq no_key_detected
+
+                // Calcular offset y guardar en la tabla
+                stx TEMP_X_REG    // Guardar X temporalmente
+                sty TEMP_Y_REG    // Guardar Y temporalmente
+                
+                jsr calculate_offset_for_ascii_table
+                
+                ldy TABLE_KEY_ASCII_X_OFFSET
+                lda #1
+                sta PRESSED_KEY_TABLE,y
+                
+                ldx TEMP_X_REG    // Restaurar X
+                ldy TEMP_Y_REG    // Restaurar Y
+
+            no_key_detected:
+                iny
+                cpy #8
+                bne scan_cols_loop
+
+                inx
+                cpx #8
+                bne scan_rows_loop
+    rts
+
+
+    process_pressed_keys:
         
-        jsr calculate_offset_for_ascii_table
-        
-        ldy TABLE_KEY_ASCII_X_OFFSET
-        lda #1
-        sta PRESSED_KEY_TABLE,y
-        
-        ldx TEMP_X_REG    // Restaurar X
-        ldy TEMP_Y_REG    // Restaurar Y
-
-    no_key_detected:
-        iny
-        cpy #8
-        bne scan_cols_loop
-
-        inx
-        cpx #8
-        bne scan_rows_loop
-        rts
-
-    print_key_results:
         // Esta función imprimirá todas las teclas detectadas
 
-    check_combo_keys:
+        check_combo_keys:
 
-        ldx #47  //CMB OFFSET !! not value
-        lda PRESSED_KEY_TABLE,x
-        beq skip // A value is 0 ? skip
+            ldx #47  //CMB OFFSET !! not value
+            lda PRESSED_KEY_TABLE,x
+            beq skip // A value is 0 ? skip
 
-        ldx #16 //Cursor key
-        lda PRESSED_KEY_TABLE,x
-        beq skip // A value is 0 ? skip
+            ldx #16 //Cursor key
+            lda PRESSED_KEY_TABLE,x
+            beq skip // A value is 0 ? skip
 
-        // If not skip , means CMB + Cursor is pressed, move to left cursor
-        .break
-        rts
+            // If not skip , means CMB + Cursor is pressed, move to left cursor
+            .break
+            rts
 
-        skip:
-            //ldx #16 //check again cursor key . Here is only cursor key
-            //lda PRESSED_KEY_TABLE,x
-            //bne is_only_cursor_key // A value is == 1 ?
+            skip:
+                //ldx #16 //check again cursor key . Here is only cursor key
+                //lda PRESSED_KEY_TABLE,x
+                //bne is_only_cursor_key // A value is == 1 ?
 
-        rts
+            rts
         
     //; -------------------- Functions ---------------------------
 
