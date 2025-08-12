21d0a1c3a2fefacbab18e04c9aa50053bbb59113
Gabriel Glez <gabrielgg85@gmail.com>
2025-08-12 12:13:28 +0100

    Added sprite macros. Working on sprite lib

--- libraries/sprite_lib/sprite_lib.asm
+++ libraries/sprite_lib/sprite_lib.asm
@@ -1,3 +1,5 @@
+#import "sprite_macros/sprite_lib_macros.asm"
+
 SPRITE_LIB:
 {
 
@@ -9,44 +11,44 @@ SPRITE_LIB:
         // ----- Set up sprite 0 -----
         
         // Sprite 0 X-pos
-        lda #160
+        /*lda #160
         sta $d000
         
         // Sprite 0 Y-pos
         lda #130
-        sta $d001
+        sta $d001*/
+        sprite_set_position(0,160,130)
 
         
-        lda #BROWN
+        /*lda #BROWN
         sta $d025 //$d026 // Sprite extra color 1
 
         lda #YELLOW
-        sta $d026 //$d027 // Sprite extra color 1
+        sta $d026 //$d027 // Sprite extra color 1*/
+        sprite_set_extra_colors(GRAY,YELLOW)
         
+        // Sprite 0 color
+        /*lda #WHITE
+        sta $d027*/
+        sprite_set_color(0,WHITE)
 
-        /*lda #LIGHT_BLUE
-		sta $d022 // Screen extra color 1
-
-		lda #BROWN
-		sta $d023 // Screen extra color 2*/
-
-
-        // ----- Load sprite 0 -----
+        // Load Sprite 0
         lda #$00c0 // FRAME 0
         //lda #$00c1   // FRAME 1
         sta $07f8
         
-        // Sprite 0 color
-        lda #WHITE
-        sta $d027
 
-        // Load Sprite 0
+        /*
         lda #%00000001 
         sta $d01c // Sprite multicolor
+        */
+        sprite_load_like_multicolor(0)
 
         // Enable sprites
-	    lda #%00000001 // Sprite 0 and sprite
-	    sta $d015 // Enable sprite
+	    /*lda #%00000001 // Sprite 0 and sprite
+	    sta $d015 // Enable sprite*/
+        sprite_enable_sprite(0)
+
 
 
         pull_regs_from_stack()
---
+++ libraries/sprite_lib/sprite_macros/sprite_lib_macros.asm
@@ -0,0 +1,180 @@
+.macro sprite_set_position( sprite_number, col, row){
+
+    // Sprite 0 Y-pos
+    lda #row
+    .if(sprite_number == 0){
+        sta $d001
+    }
+    .if(sprite_number == 1){
+        sta $d003
+    }
+    .if(sprite_number == 2){
+        sta $d005
+    }
+    .if(sprite_number == 3){
+        sta $d007
+    }
+    .if(sprite_number == 4){
+        sta $d009
+    }
+    .if(sprite_number == 5){
+        sta $d00b
+    }
+    .if(sprite_number == 6){
+        sta $d00d
+    }
+    .if(sprite_number == 7){
+        sta $d00f
+    }
+
+
+    // Sprite 0 X-pos
+    lda #col
+    .if(sprite_number == 0){
+        sta $d000
+    }
+    .if(sprite_number == 1){
+        sta $d002
+    }
+    .if(sprite_number == 2){
+        sta $d004
+    }
+    .if(sprite_number == 3){
+        sta $d006
+    }
+    .if(sprite_number == 4){
+        sta $d008
+    }
+    .if(sprite_number == 5){
+        sta $d00a
+    }
+    .if(sprite_number == 6){
+        sta $d00c
+    }
+    .if(sprite_number == 7){
+        sta $d00e
+    }
+
+}
+
+
+.macro sprite_set_extra_colors(color_one,color_two){
+
+    lda #color_one
+    sta $d025 //$d026 // Sprite extra color 1
+
+    lda #color_two
+    sta $d026 //$d027 // Sprite extra color 2
+
+}
+
+.macro sprite_set_color(sprite_number,color){
+
+    lda #color
+    .if( sprite_number == 0){
+        sta $d027
+    }
+    .if( sprite_number == 1){
+        sta $d028
+    }
+    .if( sprite_number == 2){
+        sta $d029
+    }
+    .if( sprite_number == 3){
+        sta $d02a
+    }
+    .if( sprite_number == 4){
+        sta $d02b
+    }
+    .if( sprite_number == 5){
+        sta $d02c
+    }
+    .if( sprite_number == 6){
+        sta $d02d
+    }
+    .if( sprite_number == 7){
+        sta $d02e
+    }
+
+}
+
+/* Load sprite in VIC II*/
+.macro sprite_load_like_multicolor(sprite_number){
+
+    .if(sprite_number == 0){
+        lda #%00000001 
+    }
+    
+    .if(sprite_number == 1){
+        lda #%00000010
+    }
+
+    .if(sprite_number == 2){
+        lda #%00000100
+    }
+
+    .if(sprite_number == 3){
+        lda #%00001000
+    }
+
+    .if(sprite_number == 4){
+        lda #%00010000 
+    }
+
+    .if(sprite_number == 5){
+        lda #%00100000 
+    }
+
+    .if(sprite_number == 6){
+        lda #%01000000
+    }
+
+    .if(sprite_number == 7){
+        lda #%10000000
+    }
+
+    // Load sprite like multicolor
+    sta $d01c // Sprite multicolor
+
+}
+
+
+.macro sprite_enable_sprite(sprite_number){
+
+.if(sprite_number == 0){
+        lda #%00000001 
+    }
+    
+    .if(sprite_number == 1){
+        lda #%00000010
+    }
+
+    .if(sprite_number == 2){
+        lda #%00000100
+    }
+
+    .if(sprite_number == 3){
+        lda #%00001000
+    }
+
+    .if(sprite_number == 4){
+        lda #%00010000 
+    }
+
+    .if(sprite_number == 5){
+        lda #%00100000 
+    }
+
+    .if(sprite_number == 6){
+        lda #%01000000
+    }
+
+    .if(sprite_number == 7){
+        lda #%10000000
+    }
+
+    // Enable sprite
+    sta $d015 
+
+
+}
