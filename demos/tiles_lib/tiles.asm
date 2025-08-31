59ef08c2ce424b4aa2295f4528b2b0f1a9fef104
Gabriel Glez <gabrielgg85@gmail.com>
2025-08-31 14:01:42 +0100

    Added macro for print tile

--- demos/tiles_lib/tiles.asm
+++ demos/tiles_lib/tiles.asm
@@ -1,18 +1,12 @@
+/*Insert title screen */
 insert_text(1,1,tileslib_str,YELLOW)
 
+/* Print random tiles */
+/* The position of the tile , is the index of the panel "Tile Set" in CharPad
+   the col and row are the position in screen
+*/
 
-/* Load the new memory map configuration */
-/*lda #3
-sta TILE_NUMBER
-
-lda #5
-sta TILE_COL
-
-lda #10
-sta TILE_ROW
-
-jsr TILES_LIB.print_tile*/
-
+/* Tile Number, Colum , Row */
 print_tile(0,8,12)
 print_tile(2,10,18)
 print_tile(3,12,24)
